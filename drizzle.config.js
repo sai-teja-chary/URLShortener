4868957ede85fae9a596d0b56789e8ad53466b3a
Sai Teja Chary Thummalapally <saitejachary1411@gmail.com>
2026-02-13 16:55:14 +0530

    fix: update database credentials configuration in drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -6,6 +6,13 @@ export default defineConfig({
   schema: './drizzle/schema.js',
   dialect: 'mysql',
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    host: process.env.MYSQL_HOST,
+    port: Number(process.env.MYSQL_PORT),
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 });
